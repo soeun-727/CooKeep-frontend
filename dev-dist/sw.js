972e6b60351be9da587d8a99dc62a917ab7f7773
ji070 <jiye2705@gmail.com>
2026-04-18 02:13:41 +0900

    fix: 단위,저장공간 변경 시 정보 선택에 따른 색상 변화 오류 해결

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.qfop7slubf8"
+    "revision": "0.73aei37v9s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
