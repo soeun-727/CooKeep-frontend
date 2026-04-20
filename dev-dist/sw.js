e1137e3ee17705d178d3312de1cc8f68ec12ada6
ji070 <jiye2705@gmail.com>
2026-04-20 18:00:24 +0900

    fix: 이용약관 텍스트 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.92qorepai58"
+    "revision": "0.8250cmfo2t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
