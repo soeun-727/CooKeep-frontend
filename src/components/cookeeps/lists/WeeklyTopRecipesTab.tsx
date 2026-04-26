c634d2454553949662eb8375121df2e9db25bc42
ji070 <jiye2705@gmail.com>
2026-04-26 21:35:40 +0900

    fix: 쿠킵스 전체보기 이번주 인기 부분 ui수정

--- src/components/cookeeps/lists/WeeklyTopRecipesTab.tsx
+++ src/components/cookeeps/lists/WeeklyTopRecipesTab.tsx
@@ -20,18 +20,7 @@ export default function WeeklyTopRecipesTab() {
     fetch();
   }, []);
 
-  const top3 = Array.from({ length: 3 }, (_, i) => {
-    return (
-      recipes[i] || {
-        dailyRecipeId: i,
-        rank: i + 1,
-        title: "레시피가 없습니다",
-        likeCount: 0,
-        recipeImageUrl: tempImage,
-        nickname: "",
-      }
-    );
-  });
+  const top3 = recipes.slice(0, 3);
 
   const getRankStyle = (rank: number) => {
     if (rank === 1) return "bg-[#32E389] text-white";
@@ -42,57 +31,65 @@ export default function WeeklyTopRecipesTab() {
   return (
     <div className="mt-[18px] flex justify-center">
       <div className="w-[361px] flex flex-col  ">
-        {top3.map((item) => (
-          <div
-            key={item.dailyRecipeId}
-            onClick={() =>
-              navigate(`/cookeeps/${item.dailyRecipeId}?tab=weekly`)
-            }
-            className="flex flex-col gap-[12px] p-[10px_8px] rounded-[6px] cursor-pointer"
-          >
-            {/* 1. 제목 + 좋아요 */}
-            <div className="flex items-center gap-[14px]">
-              {/* 순위 */}
-              <div
-                className={`w-[30px] h-[20px] flex items-center justify-center rounded-full text-[12px] font-semibold ${getRankStyle(item.rank)}`}
-              >
-                {item.rank}
+        {recipes.length === 0 ? (
+          // 레시피 없을 때
+          <div className="h-[200px] flex items-center justify-center text-[14px] text-[#7D7D7D]">
+            아직 등록된 레시피가 없어요
+          </div>
+        ) : (
+          // 레시피 있을 때
+          top3.map((item) => (
+            <div
+              key={item.dailyRecipeId}
+              onClick={() =>
+                navigate(`/cookeeps/${item.dailyRecipeId}?tab=weekly`)
+              }
+              className="flex flex-col gap-[12px] p-[10px_8px] rounded-[6px] cursor-pointer"
+            >
+              {/* 1. 제목 + 좋아요 */}
+              <div className="flex items-center gap-[14px]">
+                {/* 순위 */}
+                <div
+                  className={`w-[30px] h-[20px] flex items-center justify-center rounded-full text-[12px] font-semibold ${getRankStyle(item.rank)}`}
+                >
+                  {item.rank}
+                </div>
+
+                {/* 제목 + 좋아요 */}
+                <div className="flex justify-between items-center flex-1">
+                  <p className="truncate text-[14px] text-[#202020] font-medium">
+                    {item.title}
+                  </p>
+
+                  <div className="flex items-center gap-[4px]">
+                    <img src={likeGray} className="w-[18px] h-[18px]" />
+                    <span className="text-[12px] text-[#7D7D7D]">
+                      {item.likeCount}
+                    </span>
+                  </div>
+                </div>
               </div>
 
-              {/* 제목 + 좋아요 */}
-              <div className="flex justify-between items-center flex-1">
-                <p className="truncate text-[14px] text-[#202020] font-medium">
-                  {item.title}
-                </p>
-
-                <div className="flex items-center gap-[4px]">
-                  <img src={likeGray} className="w-[18px] h-[18px]" />
-                  <span className="text-[12px] text-[#7D7D7D]">
-                    {item.likeCount}
+              {/* 2. 이미지 */}
+              <div
+                className="h-[160px] rounded-[6px] bg-cover bg-center relative"
+                style={{
+                  backgroundImage: `url(${item.recipeImageUrl || tempImage})`,
+                }}
+              >
+                {/* 유저 뱃지 */}
+                <div className="absolute top-2 left-2 flex items-center h-[20px] gap-[4px]  px-[12px] rounded-full bg-black/60">
+                  <span className="text-[#32E389] text-[12px] font-medium">
+                    {item.nickname}
+                  </span>
+                  <span className="text-white text-[12px] font-medium ">
+                    님의 레시피
                   </span>
                 </div>
               </div>
             </div>
-
-            {/* 2. 이미지 */}
-            <div
-              className="h-[160px] rounded-[6px] bg-cover bg-center relative"
-              style={{
-                backgroundImage: `url(${item.recipeImageUrl || tempImage})`,
-              }}
-            >
-              {/* 유저 뱃지 */}
-              <div className="absolute top-2 left-2 flex items-center h-[20px] gap-[4px]  px-[12px] rounded-full bg-black/60">
-                <span className="text-[#32E389] text-[12px] font-medium">
-                  {item.nickname || "user 01"}
-                </span>
-                <span className="text-white text-[12px] font-medium ">
-                  님의 레시피
-                </span>
-              </div>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
