f6cb1931eb5c61084d8ab9bf21c0be95d023a5b0
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-05-06 23:24:10 +0900

    fix: height 이슈 해결

--- src/components/fixed/MainHeader.tsx
+++ src/components/fixed/MainHeader.tsx
@@ -20,7 +20,7 @@ const MainHeader = ({ isAllView }: MainHeaderProps) => {
   };
 
   return (
-    <header className="top-0 left-0 z-50 pointer-events-none w-full max-w-[450px]">
+    <header className="pt-safe top-0 left-0 z-50 pointer-events-none w-full max-w-[450px]">
       <div className="mx-auto max-w-[450px] pointer-events-auto">
         <div className="h-12 flex items-center justify-between pb-1 bg-[#FAFAFA]">
           <div className="flex-1 flex">
