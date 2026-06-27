20064f3f304e880577620f2459b60f25d55b261d
lemoncurdyogurt <suhosinsu1@naver.com>
2026-06-28 02:31:25 +0900

    feat: 절대경로 선언 코드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -110,4 +111,10 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@public": path.resolve(__dirname, "./public"),
+    },
+  },
 });
