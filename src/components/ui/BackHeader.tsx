651a711bf852109b24ed1aa309c7b17e27ef805d
ji070 <jiye2705@gmail.com>
2026-07-07 22:31:07 +0900

    fix: 백헤더 주석 제거

--- src/components/ui/BackHeader.tsx
+++ src/components/ui/BackHeader.tsx
@@ -7,19 +7,6 @@ interface BackHeaderProps {
 
 export default function BackHeader({ title, onBack }: BackHeaderProps) {
   return (
-    // <header className="bg-background fixed top-0 z-50 flex h-12 w-full max-w-[450px] items-center px-4 py-4">
-    //   <button
-    //     type="button"
-    //     onClick={onBack}
-    //     className="flex h-8 w-8 items-center justify-center"
-    //   >
-    //     <img src={BackIcon} alt="뒤로가기" />
-    //   </button>
-
-    //   <h1 className="absolute left-1/2 -translate-x-1/2 text-[16px] font-semibold">
-    //     {title}
-    //   </h1>
-    // </header>
     <header className="bg-background fixed top-0 z-50 flex h-10 w-full max-w-[450px] items-center justify-between px-4 py-2">
       <button
         type="button"
