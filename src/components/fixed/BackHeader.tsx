110db621ee674d54679970bdef0c648ac41d5e8d
lemoncurdyogurt <suhosinsu1@naver.com>
2026-07-07 17:06:09 +0900

    feat: 재료 전체 보기 헤더와 정렬 모달 추가

---
+++ src/components/fixed/BackHeader.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+import { useIngredientStore } from "@/stores/useIngredientStore";
+
+import BackIcon from "@/assets/back.svg?react";
+import SortIcon from "@/assets/fridge/sort.svg?react";
+
+import { SortDropdown } from "../fridge/features/SortDropdown";
+
+interface BackHeaderProps {
+  title: string;
+  sortIcon: boolean;
+}
+
+export const BackHeader = ({ title, sortIcon }: BackHeaderProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { setSortOrder } = useIngredientStore();
+
+  return (
+    <section className="relative flex items-center justify-between py-2">
+      {/* 뒤로가기 버튼 */}
+      <button type="button" onClick={() => window.history.back()}>
+        <BackIcon className="h-10 w-10" />
+      </button>
+
+      {/* 타이틀 */}
+      <p className="typo-l-strong">{title}</p>
+
+      {/* 정렬 영역 */}
+      <div className="relative flex h-10 w-10 items-center justify-center">
+        {sortIcon && (
+          <>
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="relative z-10 focus:outline-none"
+            >
+              <SortIcon className="h-10 w-10" />
+            </button>
+
+            {/* 분리한 드롭다운 컴포넌트 장착 */}
+            <SortDropdown
+              isOpen={isMenuOpen}
+              onSelect={option => {
+                setSortOrder(option);
+                setIsMenuOpen(false);
+              }}
+            />
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
