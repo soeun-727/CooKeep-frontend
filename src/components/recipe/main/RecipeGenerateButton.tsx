30317daf3baf76e96891e84f7d38d1aa67fde13d
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-07-09 23:39:30 +0900

    feat: 버튼 클릭 시 UI 변화 반영

--- src/components/recipe/main/RecipeGenerateButton.tsx
+++ src/components/recipe/main/RecipeGenerateButton.tsx
@@ -1,4 +1,5 @@
 import ArrowIcon from "@/assets/cookeeps/arrow.svg?react";
+import { useState } from "react";
 
 interface RecipeGenerateButtonProps {
   image: string;
@@ -13,22 +14,37 @@ export default function RecipeGenerateButton({
   description,
   onClick,
 }: RecipeGenerateButtonProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      onClick();
+      setIsClicked(false);
+    }, 300);
+  };
+
   return (
     <button
-      onClick={onClick}
-      className="rounded-L border-gray-10 flex w-full items-center gap-3 border px-3 py-4"
+      onClick={handleClick}
+      className={`rounded-L flex w-full items-center gap-3 border px-3 py-4 ${isClicked ? "bg-green-light border-green-deep" : "border-gray-10"}`}
     >
       <div className="h-25 w-25">
         <img src={image} alt="image" className="h-full w-full object-contain" />
       </div>
       <div className="flex flex-1 flex-col items-start gap-1 text-start">
-        <div className="typo-l-strong text-gray-80">{title}</div>
+        <div
+          className={`typo-l-strong ${isClicked ? "text-green-deep" : "text-gray-80"}`}
+        >
+          {title}
+        </div>
         <div className="typo-m whitespace-pre-wrap text-gray-50">
           {description}
         </div>
       </div>
       <div className="flex h-6 w-6 items-center justify-center">
-        <ArrowIcon className="text-gray-30 w-2" />
+        <ArrowIcon
+          className={`w-2 ${isClicked ? "text-green-deep" : "text-gray-30"}`}
+        />
       </div>
     </button>
   );
