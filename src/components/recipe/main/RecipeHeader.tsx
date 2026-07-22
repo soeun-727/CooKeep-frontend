b01a247f440994273338752bc971e0af14eb446e
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-07-22 17:33:28 +0900

    feat: 레시피탭 헤더 수정

--- src/components/recipe/main/RecipeHeader.tsx
+++ src/components/recipe/main/RecipeHeader.tsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useRecipeFlowStore } from "@/stores/useRecipeFlowStore";
 import { useRecipeStore } from "@/stores/useRecipeStore";
 
+import BackIcon from "@/assets/back.svg?react";
 import MenuIcon from "@/assets/recipe/main/menu.svg?react";
 import HeartIcon from "@/assets/icons/heart.svg?react";
 
@@ -37,37 +38,44 @@ export default function RecipeHeader({ title }: RecipeHeaderProps) {
   return (
     <>
       <header className="fixed top-0 right-0 left-0 z-50 mx-auto flex h-10 w-full max-w-[450px] items-center justify-between px-4 py-2">
-        {/* 왼쪽: 사이드바 버튼 */}
-        <button
-          onClick={toggleSidebar}
-          className="z-10 flex h-10 w-10 cursor-pointer items-center justify-center"
-        >
-          <MenuIcon width={40} height={40} />
-        </button>
-
-        {/* 중앙: 타이틀 */}
         {title && (
-          <div className="pointer-events-none flex items-center justify-center">
-            <h1 className="text-gray-80 typo-l-strong whitespace-nowrap">
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+            <h1 className="typo-l-strong text-gray-80 whitespace-nowrap">
               {title}
             </h1>
           </div>
         )}
 
-        {currentSessionId && (
+        {/* 왼쪽: 사이드바 버튼 */}
+        <button
+          onClick={toggleSidebar}
+          className="z-10 flex h-10 w-10 cursor-pointer items-center justify-start"
+        >
+          <BackIcon width={21} height={20} />
+        </button>
+
+        <div className="flex">
+          {currentSessionId && (
+            <button
+              onClick={handleToggleLike}
+              className="z-10 flex h-10 w-10 cursor-pointer items-center justify-end"
+            >
+              <HeartIcon
+                width={24}
+                height={21}
+                className={`stroke-gray-10 fill-current ${
+                  isLiked ? "text-gray-30" : "text-transparent"
+                }`}
+              />
+            </button>
+          )}
           <button
-            onClick={handleToggleLike}
+            onClick={toggleSidebar}
             className="z-10 flex h-10 w-10 cursor-pointer items-center justify-end"
           >
-            <HeartIcon
-              width={24}
-              height={21}
-              className={`stroke-gray-10 fill-current ${
-                isLiked ? "text-gray-30" : "text-transparent"
-              }`}
-            />
+            <MenuIcon width={22} height={22} />
           </button>
-        )}
+        </div>
       </header>
 
       {/* 사이드바 */}
