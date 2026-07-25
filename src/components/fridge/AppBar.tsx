cfc3b526fbedf1caa02821c92562029121c940c0
ji070 <jiye2705@gmail.com>
2026-07-26 01:13:22 +0900

    feat: 설정 이동 경로 넣기 & 알림설정 주석 처리

--- src/components/fridge/AppBar.tsx
+++ src/components/fridge/AppBar.tsx
@@ -1,11 +1,19 @@
-import MailNotiIcon from "@/assets/fridge/AppBar/mail_notification.svg?react";
+// import MailNotiIcon from "@/assets/fridge/AppBar/mail_notification.svg?react";
 import SettingIcon from "@/assets/fridge/AppBar/setting.svg?react";
+import { useNavigate } from "react-router-dom";
 
 export const AppBar = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-end">
-      <MailNotiIcon className="h-10 w-10" />
-      <SettingIcon className="h-10 w-10" />
+      {/* <MailNotiIcon className="h-10 w-10" /> */}
+      <button
+        type="button"
+        onClick={() => navigate("/settings")}
+        aria-label="설정"
+      >
+        <SettingIcon className="h-10 w-10" />
+      </button>
     </div>
   );
 };
