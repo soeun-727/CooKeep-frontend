85c14bd7f0bfae70e69dae290bf325e606615976
lemoncurdyogurt <suhosinsu1@naver.com>
2026-07-26 20:48:17 +0900

    fix: profile content gap오류 수정

--- src/components/myCookeep/fixed/ProfileContent.tsx
+++ src/components/myCookeep/fixed/ProfileContent.tsx
@@ -30,39 +30,43 @@ export const ProfileContent = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {/* 식물 사진 및 수정 버튼 */}
-      <div className="relative">
-        <img
-          src={profile.profilePlantImageUrl || groundImg}
-          alt="profileBackground"
-          loading="eager"
-          decoding="async"
-          onLoad={() => setImgLoaded(true)}
-          className={`h-25 w-25 w-full rounded-full object-cover transition-opacity duration-200 ${
-            imgLoaded ? "opacity-100" : "opacity-0"
-          }`}
-        />
-        <button
-          className="bg-gray-0 text-gray-30 absolute top-[70px] right-[0.5px] flex h-[30px] w-[30px] items-center justify-center rounded-full"
-          onClick={() => {
-            setIsEditModalOpen(true);
-          }}
-        >
-          <EditIcon className="text-gray-30 h-6 w-6" />
-        </button>
-      </div>
+    <div className="flex w-full flex-col items-center justify-center gap-4">
+      <div className="flex w-full flex-col items-center justify-center gap-2">
+        {/* 식물 사진 및 수정 버튼 */}
+        <div className="relative">
+          <img
+            src={profile.profilePlantImageUrl || groundImg}
+            alt="profileBackground"
+            loading="eager"
+            decoding="async"
+            onLoad={() => setImgLoaded(true)}
+            className={`h-25 w-25 w-full rounded-full object-cover transition-opacity duration-200 ${
+              imgLoaded ? "opacity-100" : "opacity-0"
+            }`}
+          />
+          <button
+            className="bg-gray-0 text-gray-30 absolute top-[70px] right-[0.5px] flex h-[30px] w-[30px] items-center justify-center rounded-full"
+            onClick={() => {
+              setIsEditModalOpen(true);
+            }}
+          >
+            <EditIcon className="text-gray-30 h-6 w-6" />
+          </button>
+        </div>
 
-      {/* 유저 정보 */}
-      <div className="flex w-full flex-col items-center gap-1">
-        <p className="typo-h3 text-gray-80">{profile?.nickname || "쿠킵이"}</p>
-        <span className="typo-label flex gap-1">
-          <p className="text-gray-30">지금은 </p>
-          <p className="text-green-deep">
-            {profile?.growingPlantName || "요리 실력을"}
+        {/* 유저 정보 */}
+        <div className="flex w-full flex-col items-center gap-1">
+          <p className="typo-h3 text-gray-80">
+            {profile?.nickname || "쿠킵이"}
           </p>
-          <p className="text-gray-30">키우는 중!</p>
-        </span>
+          <span className="typo-label flex gap-1">
+            <p className="text-gray-30">지금은 </p>
+            <p className="text-green-deep">
+              {profile?.growingPlantName || "요리 실력을"}
+            </p>
+            <p className="text-gray-30">키우는 중!</p>
+          </span>
+        </div>
       </div>
 
       <span className="flex items-center justify-center gap-[2px] rounded-full bg-[#F4F9F4] px-3 py-1 shadow-[0_4px_16px_0_rgba(25,95,54,0.10)]">
