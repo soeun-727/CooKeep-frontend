7a0b54b1cffaefaef2d1cade49aa825a003d7ea5
lemoncurdyogurt <suhosinsu1@naver.com>
2026-08-02 00:55:53 +0900

    fix: 내부 렌더되는 영역 w-full 적용

--- src/components/ui/BottomTabBar.tsx
+++ src/components/ui/BottomTabBar.tsx
@@ -11,9 +11,9 @@ export const BottomTabBar = ({
     <div className="fixed inset-0 z-200 flex items-end justify-center">
       <div className="bg-gray-80/50 absolute inset-0" onClick={onClose} />
 
-      <section className="bg-gray-0 animate-slide-up relative flex w-full max-w-[450px] flex-col gap-6 rounded-t-[16px] px-4 pt-6">
+      <section className="bg-gray-0 animate-slide-up relative flex w-full max-w-[450px] flex-col items-center justify-center gap-6 rounded-t-[16px] px-4 pt-6">
         <h1 className="typo-l-strong text-gray-80">{title}</h1>
-        <div>{BottomTabBarContent}</div>
+        <div className="w-full">{BottomTabBarContent}</div>
       </section>
     </div>
   );
