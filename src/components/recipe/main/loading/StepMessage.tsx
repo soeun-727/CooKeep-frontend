0bd32f7fe65d5af1de0fad913da54b99ca4f2a93
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-08-02 23:47:03 +0900

    fix: 배경 색 변경

--- src/components/recipe/main/loading/StepMessage.tsx
+++ src/components/recipe/main/loading/StepMessage.tsx
@@ -5,7 +5,7 @@ interface StepMessageProps {
 
 export default function StepMessage({ message, icon }: StepMessageProps) {
   return (
-    <div className="step-message border-gray-10 rounded-M flex items-center gap-3 border p-3">
+    <div className="step-message bg-gray-0 border-gray-10 rounded-M flex items-center gap-3 border p-3">
       <img src={icon} className="h-6 w-6" alt="check" />
       <span className="text-gray-80 typo-m flex-1 text-left">{message}</span>
     </div>
