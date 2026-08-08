c571684a9aa2edb2085631c3c0b3d9bebcaf7c72
ji070 <jiye2705@gmail.com>
2026-08-08 10:29:31 +0900

    fix: watertoast ui 수정

--- src/components/cookeeps/plant/WaterToast.tsx
+++ src/components/cookeeps/plant/WaterToast.tsx
@@ -9,7 +9,7 @@ export default function WaterToast({ message, isVisible }: WaterToastProps) {
   return (
     <div className="pointer-events-none absolute top-[44%] left-1/2 -translate-x-1/2 translate-x-[3px]">
       {/* 식물 PNG의 실제 시각 중심에 맞추기 위한 위치 보정 */}
-      <div className="bg-gray-0/75 text-green-deep animate-fade-in-out shadow-plant inline-flex items-center gap-2 rounded-full px-5 py-1 text-[12px] font-bold">
+      <div className="bg-gray-0/80 text-green-deep animate-fade-in-out typo-caption shadow-toast inline-flex items-center justify-center gap-2 rounded-full px-4 py-1">
         {message}
       </div>
     </div>
