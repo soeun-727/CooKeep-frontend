c7766149d1f3d0a85b7df55f649a902de17c5713
lemoncurdyogurt <suhosinsu1@naver.com>
2026-08-10 01:41:29 +0900

    refactor: 요리기록 렌더컨텐트 ui 수정사항 반영

--- src/components/myCookeep/record/RecordEntry.tsx
+++ src/components/myCookeep/record/RecordEntry.tsx
@@ -3,17 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { DailyRecipe } from "@/api/myRecipe";
 import { useCookeepRecordStore } from "@/stores/useCookeepRecordStore";
 
-import tempFoodPhoto from "@/assets/mycookeep/record/temp_food_photo.svg";
+import PlusIcon from "@/assets/fridge/items/plus.svg?react";
+import PotIcon from "@/assets/mycookeep/pot.svg?react";
 
-import Button from "@/components/ui/Button";
-
-import AddRecordButton from "./AddRecordButton";
 import RecordCard from "./RecordCard";
 
 interface RecordEntryProps {
-  records: DailyRecipe[];
+  record?: DailyRecipe;
 }
-export default function RecordEntry({ records }: RecordEntryProps) {
+export default function RecordEntry({ record }: RecordEntryProps) {
   const navigate = useNavigate();
   const { resetRecord } = useCookeepRecordStore();
 
@@ -21,40 +19,43 @@ export default function RecordEntry({ records }: RecordEntryProps) {
     resetRecord();
     navigate("/mycookeep/record/select");
   };
-
   return (
     <div className="relative">
-      <div className="bg-gray-0 flex items-center justify-center rounded-b-[6px] px-4 pt-[50px] pb-[calc(72px+env(safe-area-inset-bottom))]">
-        <div className="flex w-full max-w-[361px] flex-col items-start gap-4">
-          {records.length === 0 ? (
-            <>
-              <img
-                src={tempFoodPhoto}
-                alt="임시 요리 이미지"
-                className="h-[160px] w-full object-contain"
-              />
-              <div className="flex w-full flex-col items-center gap-[6px]">
-                <Button size="L" variant="black" onClick={handleRecordClick}>
-                  오늘 만든 요리 기록하기
-                </Button>
-              </div>
-            </>
-          ) : (
-            records.map(record => (
-              <RecordCard key={record.dailyRecipeId} record={record} />
-            ))
-          )}
-        </div>
-      </div>
-      {records.length > 0 && (
-        <div className="pointer-events-none fixed inset-x-0 bottom-[calc(84px+env(safe-area-inset-bottom))] z-[100]">
-          <div className="relative mx-auto w-full max-w-[450px]">
-            <div className="pointer-events-auto absolute right-[31px] bottom-0">
-              <AddRecordButton />
+      <div className="flex w-full flex-col items-start gap-2">
+        <h1 className="typo-l-strong text-gray-80 w-full px-1">
+          최근에 한 요리
+        </h1>
+
+        {record ? (
+          <RecordCard key={record.dailyRecipeId} record={record} />
+        ) : (
+          <div className="border bg-gray-0 border-gray-10 rounded-L flex gap-4 px-3 py-4">
+            <figure className="bg-gray-10 flex py-[18px] pr-[15px] pl-[11px]">
+              <PotIcon className="text-gray-30 h-16 w-16" />
+            </figure>
+
+            <div className="flex flex-1 flex-col items-start justify-center gap-[2px]">
+              <p className="typo-l-strong text-gray-80">
+                아직 기록된 요리가 없어요
+              </p>
+              <p className="typo-m text-gray-50">
+                요리를 기록하고
+                <br /> 쿠키를 받아보세요!
+              </p>
             </div>
           </div>
-        </div>
-      )}
+        )}
+
+        <button
+          className="rounded-M flex w-full gap-3 bg-white p-3"
+          onClick={handleRecordClick}
+        >
+          <div className="flex h-6 w-6 items-center justify-center rounded-full bg-white">
+            <PlusIcon className="text-green h-[10px] w-[10px]" />
+          </div>
+          <p className="typo-m-strong text-gray-80">오늘 만든 요리 기록하기</p>
+        </button>
+      </div>
     </div>
   );
 }
