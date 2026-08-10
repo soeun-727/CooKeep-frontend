8204c6fe8126b4984e423feaa40b911652eee7e4
ji070 <jiye2705@gmail.com>
2026-08-10 16:49:20 +0900

    fix: watertoast 위치 수정

--- src/components/cookeeps/plant/WaterToast.tsx
+++ src/components/cookeeps/plant/WaterToast.tsx
@@ -7,7 +7,7 @@ export default function WaterToast({ message, isVisible }: WaterToastProps) {
   if (!isVisible) return null;
 
   return (
-    <div className="pointer-events-none absolute top-[44%] left-1/2 -translate-x-1/2 translate-x-[3px]">
+    <div className="pointer-events-none absolute top-[50%] left-1/2 -translate-x-1/2 translate-x-[3px]">
       {/* 식물 PNG의 실제 시각 중심에 맞추기 위한 위치 보정 */}
       <div className="bg-gray-0/80 text-green-deep animate-fade-in-out typo-caption shadow-toast inline-flex items-center justify-center gap-2 rounded-full px-4 py-1">
         {message}
