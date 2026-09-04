53ae627683af3a355835074798a59ddcedc15044
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-09-04 14:14:09 +0900

    fix: 설치 가이드 설명문 색상 수정

--- src/components/auth/onboarding/InstallGuideDescription.tsx
+++ src/components/auth/onboarding/InstallGuideDescription.tsx
@@ -7,7 +7,7 @@ export default function InstallGuideDescription({
 }: InstallGuideDescriptionProps) {
   if (isAndroid) {
     return (
-      <div className="flex w-full flex-col items-center self-stretch text-center text-gray-50">
+      <div className="flex w-full flex-col items-center self-stretch text-center text-[#7D7D7D]">
         <p className="typo-m-strong">설치 없이 바로 시작하고 싶다면?</p>
         <p className="typo-m">Google Chrome 우측 상단 메뉴 [홈 화면에 추가]</p>
       </div>
@@ -15,7 +15,7 @@ export default function InstallGuideDescription({
   }
 
   return (
-    <div className="flex w-full flex-col items-center gap-3 self-stretch text-center text-gray-50">
+    <div className="flex w-full flex-col items-center gap-3 self-stretch text-center text-[#7D7D7D]">
       <p className="typo-l-strong">
         iOS 사용자는 Safari에서 열어주세요
         <br />
